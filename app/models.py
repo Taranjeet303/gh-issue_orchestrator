a3c8cb39fd1b27c119dbbe001013bea4ce1943eb
Taranjeet303 <taranjeetkaurtk303@gmail.com>
2026-06-19 20:08:52 +0530

    feat: add workflow and workflow step database models

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column,String,Text,DateTime,Integer
+from sqlalchemy import Column,String,Text,DateTime,Integer,Boolean, ForeignKey,JSON
 from datetime import datetime
+from sqlalchemy.orm import relationship
 from app.database import Base
 class GitHubEvent(Base):
     __tablename__ = "github_events"
@@ -14,3 +15,27 @@ class GitHubEvent(Base):
     label =   Column(String)
     status  =      Column(String, default="pending")
     received_at = Column(DateTime, default=datetime.utcnow)
+
+class Workflow(Base):
+        __tablename__= "workflows"
+
+        id= Column(Integer, primary_key=True)
+        name= Column(String)
+        trigger_event= Column(String)
+        trigger_label= Column(String)
+        is_active= Column(Boolean, default=True)
+        created_at=Column(DateTime, default=datetime.utcnow)
+        
+        steps= relationship("WorkflowStep",back_populates="workflow")
+
+class WorkflowStep(Base):
+        __tablename__="workflow_steps" 
+
+        id=Column(Integer, primary_key=True)
+        workflow_id=Column(Integer,ForeignKey("workflows.id"))
+        step_order=Column(Integer)
+        action_type=Column(String)
+        config=Column(JSON)
+        retry_limit=Column(Integer, default=3)
+        created_at=Column(DateTime, default=datetime.utcnow)     
+        workflow= relationship("Workflow", back_populates="steps")
