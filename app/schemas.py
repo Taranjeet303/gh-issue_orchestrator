4bdaa6dac0a4c84a9d39ca8c3b46cf0237153c6b
Taranjeet303 <taranjeetkaurtk303@gmail.com>
2026-06-22 13:37:39 +0530

    feat: add workflow and step schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,8 @@
 
 from pydantic import BaseModel
-
+from datetime import datetime
+from pydantic import ConfigDict
+from typing import Any
 class Label(BaseModel):
     name: str
 
@@ -17,3 +19,40 @@ class GitHubEvent(BaseModel):
     action: str
     issue: Issue
     repository: Repository
+
+class StepCreate(BaseModel):
+    step_order: int
+    action_type: str
+    config: dict[str, Any]
+    retry_limit: int = 3
+
+class StepResponse(BaseModel):
+    id: int
+    workflow_id: int
+    step_order: int
+    action_type: str
+    config: dict[str, Any]
+    retry_limit: int
+    created_at: datetime
+    model_config = ConfigDict(
+        from_attributes=True
+    )
+
+class WorkflowCreate(BaseModel):
+    name:str
+    trigger_event: str
+    trigger_label: str
+    
+
+class WorkflowResponse(BaseModel):
+    id:int
+    name: str
+    trigger_event: str
+    trigger_label: str
+    is_active: bool
+    created_at: datetime
+    steps: list[StepResponse] = []
+    model_config = ConfigDict(
+        from_attributes=True
+    )
+
